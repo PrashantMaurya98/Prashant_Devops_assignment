2a7c9dee2ca14ffb601b05b27b2c5c0daada6883
PrashantMaurya98 <67199579+PrashantMaurya98@users.noreply.github.com>
2024-11-23 19:34:05 +0530

    Update PrashantApplication.java

--- AssignmentDevOps/src/main/java/com/prashant/PrashantApplication.java
+++ AssignmentDevOps/src/main/java/com/prashant/PrashantApplication.java
@@ -1,6 +1,5 @@
 package com.prashant;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @Controller
 
-@SpringBootApplication
 public class PrashantApplication extends SpringBootServletInitializer {
 
     @Override
@@ -22,9 +20,6 @@ public class PrashantApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PrashantApplication.class, args); // Updated class name
     }
-}
-
-public class PrashantApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
@@ -33,4 +28,5 @@ public class PrashantApplication {
         return "index";
     }
 }
+
 
