4f432125f2cd31f750e1dacef6d99baad4a849b1
PrashantMaurya98 <67199579+PrashantMaurya98@users.noreply.github.com>
2024-11-23 13:50:49 +0530

    Update StartApplication.java

--- ScoremeAssignment/AssignmentDevOps/src/main/java/com/prashant/StartApplication.java
+++ ScoremeAssignment/AssignmentDevOps/src/main/java/com/prashant/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "This is a Java Static Web Application");
+        model.addAttribute("title", "This is a Java based Static Web Application");
         model.addAttribute("msg", "Application Is Deployed To Apache Tomcat");
         return "index";
     }
